e8615c2d243b893a88c1e1f112b5641d1c401354
Adithya Neelakantan <67263572+neelstrongarm@users.noreply.github.com>
2020-07-06 11:02:32 +0530

    final commit

--- Windows/app.py
+++ Windows/app.py
@@ -15,12 +15,17 @@ def index():
 
 @app.route('/output', methods=["GET", "POST"])
 def output():
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-        inp = r.recognize_google(audio)
-        # print(inp)
-        fin = operation(inp.lower())
-        return render_template("output.html", ans = inp, fin = fin)
+    try:
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+            inp = r.recognize_google(audio)
+            fin = operation(inp.lower())
+            if fin != 'error':
+                return render_template("output.html", ans = inp, fin = fin)
+            else:
+                return render_template("apology.html")
+    except Exception:
+        return render_template("apology.html")
 
 def isDigit(x):
     try:
@@ -38,12 +43,14 @@ def numbers(inp):
         if "%" in i:
             num.append(float(i[:len(i)-1]))
         if "/" in i:
-            x = list(map(float, i.split("/")))
-            num.extend(x)
-    print(num)
+            if all(list(map(isDigit, i.split('/')))):
+                x = list(map(float, i.split("/")))
+                num.extend(x)
     return num
 
 def voice_out(num_mytext):
+    if num_mytext == "error":
+        return "error"
     if num_mytext == int(num_mytext):
         mytext = str(int(num_mytext))
     else:
@@ -53,39 +60,42 @@ def voice_out(num_mytext):
     filename = 'output.mp3'
     myobj.save(filename)
     playsound.playsound(filename)
-    print(mytext)
+    os.remove("output.mp3")
+    return mytext
 
 def operation(inp):    
     inp = inp.replace("minus ", " -")
     inp = inp.replace("negative ", " -")
-    print(inp)
     num = numbers(inp)
     if "percent" in inp or "percentage" in inp or "%" in inp:
-        voice_out(percent(num))
+        a=voice_out(percent(num))
     elif "mod" in inp or "modulus" in inp or "modulo" in inp or "remainder" in inp:
-        voice_out(mod(num))
+        a=voice_out(mod(num))
     elif "root" in inp:
-        voice_out(sq_root(num))
+        a=voice_out(sq_root(num))
     elif "square" in inp or "itself" in inp:
-        voice_out(square(num))
-    elif "add" in inp or "+" in inp or "plus" in inp:
-        voice_out(add(num))       
-    elif "multiply" in inp or "times" in inp or "multiplied" in inp or "into" in inp or "x" in inp or "*" in inp:
-        voice_out(multiply(num))    
-    elif "divide" in inp or "by" in inp or "/" in inp or "bye" in inp:
-        voice_out(divide(num))
+        a=voice_out(square(num))
+    elif "add" in inp or "+" in inp or "plus" in inp or "sum" in inp:
+        a=voice_out(add(num))       
+    elif "multiply" in inp or "times" in inp or "multiplied" in inp or "into" in inp or "x" in inp or "*" in inp or "product" in inp:
+        a=voice_out(multiply(num))    
+    elif "divide" in inp or "by" in inp or "/" in inp or "bye" in inp or "quotient" in inp:
+        a=voice_out(divide(num))
     elif "subtract" in inp and "from" in inp:
-        voice_out(subtract(num,flag=True)) #if flag is true num[1] - num[0]  
-    elif "-" in inp or "minus" in inp:
-        voice_out(subtract(num,flag=False)) #if flag is flase num[0] - num[1] 
+        a=voice_out(subtract(num,flag=True)) #if flag is true num[1] - num[0]  
+    elif "-" in inp or "minus" in inp or "difference" in inp:
+        a=voice_out(subtract(num,flag=False)) #if flag is flase num[0] - num[1] 
+    else:
+        a='error'
+    return a
 
 def add(num):
-    total = 0
-    for i in num:
-        total += i
+    total = sum(num)
     return total
 
 def subtract(num,flag):
+    if len(num) < 2:
+        return 'error'
     if flag == True:
         total = num[1] - num[0]
     else:
@@ -93,35 +103,39 @@ def subtract(num,flag):
     return total
 
 def multiply(num):
-    
+    if len(num) < 2:
+        return 'error'
     total = 1
     for i in num:
         total *= i
     return total
 
 def divide(num):
+    if len(num) < 2:
+        return 'error'
     total = num[0] / num[1]
     return total
 
 def mod(num):
+    if len(num) < 2:
+        return 'error'
     total = num[0] % num[1]
     return total
 
 def sq_root(num):
+    if len(num) < 1:
+        return 'error'
     total = num[0] ** (1/2)
     return total
 
 def square(num):
+    if len(num) < 1:
+        return 'error'
     total = num[0] **2
     return total
 
 def percent(num):
+    if len(num) < 2:
+        return 'error'
     total = num[0] * num[1] / 100
     return total
-
-# with sr.Microphone() as source:
-#     print("Kuch toh bolo, sharma kyu rahe ho")
-#     audio = r.listen(source)
-#     inp = r.recognize_google(audio)
-#     print(inp)
-#     operation(inp.lower())
